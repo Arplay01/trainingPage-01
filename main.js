2c091fcfc3142d7e52d8aaa5bc42acc5ee5b9937
Arek Paprzycki <arek.working@gmail.com>
2020-05-20 23:55:54 +0200

    HTML added, BEM added, most CSS added, basic JS added

---
+++ main.js
@@ -0,0 +1,25 @@
+/* alert('Cześć!') */
+
+console.log('alert')
+
+/* const createContent = (querySelectorContent, content) => {
+    const element = document.querySelector(querySelectorContent)
+    element.innerHTML = content
+}
+
+createContent('.main__heading--js','Hello World') */
+
+/* function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent)
+    element.innerHTML = content
+}
+
+createContent('.main__heading--js','Hello World') */
+
+const createContent = (querySelectorContent, content) => {
+    const element = document.querySelector(querySelectorContent)
+    element.innerHTML = content
+}
+
+createContent('.section__footer--description--js', 'Właśnie kończę na dziś (napisane w js)')
+
